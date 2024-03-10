7c8afa44ac46627418c60fdc620d0f59fdea3f65
João Manoel <jmaoliveira.cic@uesc.br>
2024-03-10 00:20:38 -0300

    fix

--- p012/src/app/components/form-user/form-user.component.ts
+++ p012/src/app/components/form-user/form-user.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { OnInit } from '@angular/core';
+import { FormDataService } from '../../form-data.service';
 
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 
@@ -12,33 +13,28 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
 export class FormUserComponent{
   userForm: FormGroup;
 
-  constructor() {
+  constructor(private formDataService: FormDataService) {
     this.userForm = new FormGroup({
-      'user_name': new FormControl(null, [Validators.required, Validators.minLength(12)]),
+      'username': new FormControl(null, [Validators.required, Validators.maxLength(12)]),
       'password': new FormControl(null, [Validators.required, Validators.minLength(4)]),
       'email': new FormControl(null, [Validators.required, Validators.email]),
       'full_name': new FormControl(null, [Validators.required]),
       'number': new FormControl(null, [Validators.required]),
-      'adress': new FormControl(null, [Validators.required]),
+      'address': new FormControl(null, [Validators.required]),
       'birthdate': new FormControl(null),
       'gender': new FormControl(null),
-      'occupation': new FormControl(null)
+      'profession': new FormControl(null)
    });
   }
 
   onSubmit() {
-    const user = {
-      user_name: this.userForm.value.user_name,
-      password: this.userForm.value.password,
-      email: this.userForm.value.email,
-      full_name: this.userForm.value.full_name,
-      number: this.userForm.value.number,
-      adress: this.userForm.value.adress,
-      birthdate: this.userForm.value.birthdate,
-      gender: this.userForm.value.gender,
-      occupation: this.userForm.value.occupation
-    }
+    console.log(this.userForm);
+    if (this.userForm.valid) {
+      this.formDataService.storeFormData(this.userForm.value);
 
-    console.log(user);
+      this.userForm.reset();
+
+      console.log(this.formDataService.getFormData());
+    }
   }
 }
